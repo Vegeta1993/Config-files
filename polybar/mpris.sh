7008a97f63dde42f999cbf5e7e63c6db05aa9d58
ahalsnad <halsna01@gw.uni-passau.de>
2017-08-17 15:37:08 +0200

    polybar added

---
+++ polybar/mpris.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+icon=""
+
+player_status=$(playerctl status 2>/dev/null)
+if [[ $? -eq 0 ]]; then
+  metadata="\"$(playerctl metadata title)\" by $(playerctl metadata artist)"
+fi
+
+if [[ $player_status = "Playing" ]]; then
+  echo "$icon $metadata"
+elif [[ $player_status = "Paused" ]]; then
+  echo "$icon $metadata"
+else
+  echo "$icon"
+fi
+
