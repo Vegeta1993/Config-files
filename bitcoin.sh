9cceec7bc6455ce0b38eb7378c1277fa4eb4663a
ahalsnad <halsna01@gw.uni-passau.de>
2017-12-16 14:34:48 +0000

    LTC added

--- bitcoin.sh
+++ bitcoin.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-getJSON(){
-json=$(curl -s 'http://realtimebitcoin.info/stats')
+getbtc(){
+btc=$(curl -s 'http://realtimebitcoin.info/stats')
 
-buy=$(echo $json|jq '.ticker|.USD|.buy')
-sell=$(echo $json|jq '.ticker|.USD|.sell')
-avg=$(echo $json|jq '.ticker|.USD|.avg')
+buy=$(echo $btc|jq '.ticker|.USD|.buy')
+sell=$(echo $btc|jq '.ticker|.USD|.sell')
+avg=$(echo $btc|jq '.ticker|.USD|.avg')
 
-notify-send 'Bitcoin:' "Buy: $buy\nSell: $sell\nAvg:$avg"
+notify-send 'Bitcoin:' "Buy: $buy\nSell: $sell\nAvg: $avg"
 }
 
+getltc(){
+ltc=$(curl -s 'https://api.coinbase.com/v2/prices/USD/spot?')
+ltc_price=$(echo $ltc|jq '.data[2]|.amount')
+ltc_real_price=$(echo $ltc_price | sed 's/"//g')
+notify-send 'Litcoin:' "Price: $ltc_real_price"
+}
 while true
 do
-  getJSON
+  getbtc
+  getltc
   sleep 300
 done
